5d8195ded24292e948b7101dfa16d523d031978f
LaurenZeigler <lzeigler131@gmail.com>
2026-04-07 23:39:34 -0600

    april7

---
+++ Unity_BugCatcher/BugCatcher/Assets/__Files/NetAction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NetAction : MonoBehaviour
+{
+    public bool isFast = false;
+    public bool isLarge = false;
+
+    public float speedDefault;
+    public float speedFast;
+    public float sizeDefault;
+    public float sizeLarge;
+
+    // public InputActionReference interactButton; (MAKE new button for slinging net)
+    private bool isNearby;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // or try if (other.CompareTag("Player") || other.CompareTag("Agent"))
+        {
+            isNearby = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isNearby = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
